7e90d52311cc77220500e994ee1174a4e5c2bee4
李忠豫 <zhongyu.li@blockc.pro>
2019-08-27 18:58:31 +0800

    提交笔试题

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,98 @@
 <template>
     <div>
-        hello home
+        <div class="btn-wrap">
+            <button @click="sortByDate">按日期排序</button>
+            <button @click="sortByType">按类型排序</button>
+        </div>
+        <div class="img-wrap">
+            <div class="box" v-for="(item,index) in list" :key= "index">
+                <div class="little">
+                    <img :src="item.url" alt="">
+                </div>
+                
+                <p>名字：<span>{{item.name}} +++ {{item.sort}}</span></p>
+                <p>创建日期<span>{{item.create_time}}</span></p>
+            </div>
+        </div>
     </div>
 </template>
+<script>
+import { data } from './data.js'
+export default {
+    data(){
+        return {
+         
+            list:[],
+            list1:null
+        }
+    },
+    mounted(){
+     this.list = data
+    },
+    methods:{
+        sortByDate(){
+            data.sort((a,b) => {
+                
+                return a.create_time<b.create_time ? 1:-1;
+            })
+            this.list = data;
+        },
+        sortByType(){
+            
+            this.list.sort(function(a,b){
+                return a.sort>b.sort?1:-1
+            }) 
+        }
+    }
+}
+</script>
+<style lang="scss">
+    * {
+        padding: 0;
+        margin: 0;
+    }
+    .btn-wrap {
+        display: flex;
+        width: 80%;
+        margin: 20px auto;
+        height: 80px;
+        border: 1px solid #eee;
+        justify-content: space-around;
+        align-items: center;
+        button {
+            width: 80px;
+            height: 40px;
+            background: darkcyan;
+            color: #fff;
+            border: none;
+            border-radius: 10px;
+            cursor: pointer;
+        }
+    }
+    .img-wrap {
+        width: 100%;
+        padding:20px;
+        display: flex;
+        flex-wrap: wrap;
+        .box {
+            width: 20%;
+            height: 300px;
+            margin: 2% 2%;
+            border:1px solid #ccc;
+            .little {
+                width:100%;
+                max-height: 240px;
+                overflow: hidden;
+                img{
+                    width: 100%;
+                }
+            }
+            p {
+                margin: 8px 0;
+                font-size: 14px;
+                text-align: left;
+                text-indent: 2em;
+            }
+        }
+    }
+</style>
---
+++ src/components/data.js
@@ -0,0 +1,114 @@
+export const data = [
+    {
+        url:'https://f11.baidu.com/it/u=3015290577,3820928928&fm=72',
+        name:'dog',
+        create_time:'2019-1-1',
+        sort:1
+    },
+    {
+        url:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3660178309,3287204877&fm=26&gp=0.jpg',
+        name:'cat',
+        create_time:'2019-1-3',
+        sort:1
+    }, {
+        url:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1852576403,7201360&fm=26&gp=0.jpg',
+        name:'mouse',
+        create_time:'2019-1-4',
+        sort:1
+    },
+    // 植物
+    {
+        url:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=603889380,2846759773&fm=26&gp=0.jpg',
+        name:'西红柿',
+        create_time:'2019-7-1',
+        sort:2
+    },
+    {
+        url:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4195624113,1653492788&fm=26&gp=0.jpg',
+        name:'茄子',
+        create_time:'2019-8-1',
+        sort:2
+    }, {
+        url:'https://f10.baidu.com/it/u=1039062744,1620762808&fm=72',
+        name:'土豆',
+        create_time:'2019-2-1',
+        sort:2
+    }, {
+        url:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=766888221,2457457173&fm=26&gp=0.jpg',
+        name:'白菜',
+        create_time:'2019-12-1',
+        sort:2
+    },
+    //汽车
+    {
+        url:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2444230726,822309735&fm=26&gp=0.jpg',
+        name:'奥迪',
+        create_time:'2019-5-10',
+        sort:3
+    }, {
+        url:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3521474023,2875404375&fm=26&gp=0.jpg',
+        name:'比亚迪',
+        create_time:'2019-10-12',
+        sort:3
+    }, {
+        url:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3113145301,2122574480&fm=26&gp=0.jpg',
+        name:'宝马',
+        create_time:'2019-11-11',
+        sort:3
+    }, {
+        url:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4217041753,1893371682&fm=26&gp=0.jpg',
+        name:'悍马',
+        create_time:'2019-4-12',
+        sort:3
+    },
+    // 花
+    {
+        url:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1864758809,335728026&fm=26&gp=0.jpg',
+        name:'pandan',
+        create_time:'2019-1-2',
+        sort:1
+    },
+    {
+        url:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1451231543,3230931281&fm=26&gp=0.jpg',
+        name:'玫瑰',
+        create_time:'2019-8-15',
+        sort:4
+    }, {
+        url:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3512918072,113730236&fm=26&gp=0.jpg',
+        name:'百合',
+        create_time:'2019-2-12',
+        sort:4
+    }, {
+        url:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1314545833,2348170097&fm=26&gp=0.jpg',
+        name:'郁金香',
+        create_time:'2013-12-1',
+        sort:4
+    }, {
+        url:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2573109594,3005878218&fm=26&gp=0.jpg',
+        name:'喇叭花',
+        create_time:'2013-1-1',
+        sort:4
+    }, 
+    // 家具
+    {
+        url:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2155572920,1115224719&fm=26&gp=0.jpg',
+        name:'桌子',
+        create_time:'2009-1-1',
+        sort:5
+    }, {
+        url:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3261476840,1430751285&fm=26&gp=0.jpg',
+        name:'椅子',
+        create_time:'2014-1-1',
+        sort:5
+    }, {
+        url:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3421068057,3424347896&fm=26&gp=0.jpg',
+        name:'柜子',
+        create_time:'2016-1-1',
+        sort:5
+    }, {
+        url:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2753807240,2358115060&fm=26&gp=0.jpg',
+        name:'床',
+        create_time:'2019-10-12',
+        sort:5
+    },
+]
